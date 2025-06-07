a37bac874baf1b93fd12060b8b0a6c0bdfeb4627
duc <thienductang@gmail.com>
2025-06-07 13:56:45 +0700

    udev

--- src/hoverboard-driver/startup/initenv.sh
+++ src/hoverboard-driver/startup/initenv.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "Creating udev rule for Hoverboard UART..."
+
 echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout", SYMLINK+="hoverboard_uart"' > /etc/udev/rules.d/hoverboard.rules
 
+# Reload udev
+echo "Reloading udev rules..."
 service udev reload
 sleep 2
 service udev restart
 
+echo "Done! You can now use /dev/hoverboard_uart."

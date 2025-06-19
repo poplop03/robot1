a6142673a0ee9bf0e611f2b59daa36e33b7c9359
duc <thienductang@gmail.com>
2025-06-19 13:12:21 +0700

    rgbd pcl

--- src/rgbd_to_pcl/src/rgbd_to_pcl.cpp
+++ src/rgbd_to_pcl/src/rgbd_to_pcl.cpp
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
@@ -9,23 +8,19 @@ ros::Publisher pub;
 
 void callback(const sensor_msgs::PointCloud2ConstPtr& input)
 {
-    // Convert to PCL format
     pcl::PointCloud<pcl::PointXYZRGB> cloud_rgb;
     pcl::fromROSMsg(*input, cloud_rgb);
 
-    // Convert to PointXYZ only
     pcl::PointCloud<pcl::PointXYZ> cloud_xyz;
     cloud_xyz.header = cloud_rgb.header;
     cloud_xyz.points.reserve(cloud_rgb.points.size());
 
     for (const auto& pt : cloud_rgb.points)
     {
-        if (pcl::isFinite(pt)) {
+        if (pcl::isFinite(pt))
             cloud_xyz.points.emplace_back(pt.x, pt.y, pt.z);
-        }
     }
 
-    // Convert back to ROS message
     sensor_msgs::PointCloud2 output;
     pcl::toROSMsg(cloud_xyz, output);
     pub.publish(output);

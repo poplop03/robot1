c35d44c722ec6f4a999e58c8bcfcdba9d76ea4e4
duc <thienductang@gmail.com>
2025-06-24 14:33:47 +0700

    nor imu

--- src/ros_imu_bno055/src/imu_ros.py
+++ src/ros_imu_bno055/src/imu_ros.py
@@ -295,9 +295,21 @@ class SensorIMU:
         imu_data.angular_velocity.y = gyroscope[1]
         imu_data.angular_velocity.z = gyroscope[2]
 
-        imu_data.orientation_covariance[0] = -1
-        imu_data.linear_acceleration_covariance[0] = -1
-        imu_data.angular_velocity_covariance[0] = -1
+        imu_data.orientation_covariance = [
+            0.01, 0.0,  0.0,
+            0.0,  0.01, 0.0,
+            0.0,  0.0,  0.01
+        ]
+        imu_data.angular_velocity_covariance = [
+            0.02, 0.0,  0.0,
+            0.0,  0.02, 0.0,
+            0.0,  0.0,  0.02
+        ]
+        imu_data.linear_acceleration_covariance = [
+            0.1,  0.0,  0.0,
+            0.0,  0.1,  0.0,
+            0.0,  0.0,  0.1
+        ]
 
         self.imu_data_seq_counter=+1
 

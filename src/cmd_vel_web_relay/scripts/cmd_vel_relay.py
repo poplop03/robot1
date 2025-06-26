461ab14facffc819acb39dbf0ca7fd875db15c1f
duc <thienductang@gmail.com>
2025-06-26 14:47:54 +0700

    stop pub when no speed

--- src/cmd_vel_web_relay/scripts/cmd_vel_relay.py
+++ src/cmd_vel_web_relay/scripts/cmd_vel_relay.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import rospy
 from geometry_msgs.msg import Twist
 
@@ -7,19 +5,25 @@ class CmdVelRelay:
     def __init__(self):
         rospy.init_node('cmd_vel_relay', anonymous=True)
 
-        # Subscribers and Publishers
         self.sub = rospy.Subscriber('/cmd_vel_web', Twist, self.cmd_vel_callback, queue_size=1)
         self.pub = rospy.Publisher('/hoverboard_velocity_controller/cmd_vel', Twist, queue_size=1)
 
-        self.latest_cmd = Twist()  # Default zero velocities
-        self.rate = rospy.Rate(20)  # 20 Hz publishing
+        self.latest_cmd = Twist()
+        self.should_publish = False
+        self.rate = rospy.Rate(20)
 
     def cmd_vel_callback(self, msg):
-        self.latest_cmd = msg
+        # Check if all components are zero
+        if (msg.linear.x == 0 and msg.linear.y == 0 and msg.angular.z == 0):
+            self.should_publish = False
+        else:
+            self.should_publish = True
+            self.latest_cmd = msg
 
     def run(self):
         while not rospy.is_shutdown():
-            self.pub.publish(self.latest_cmd)
+            if self.should_publish:
+                self.pub.publish(self.latest_cmd)
             self.rate.sleep()
 
 if __name__ == '__main__':

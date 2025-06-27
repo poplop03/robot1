2ff57ae5d309c4fde07cb298c669f0034b500b25
duc <thienductang@gmail.com>
2025-06-27 12:19:12 +0700

    urdfv6

--- src/cmd_vel_web_relay/scripts/cmd_vel_relay.py
+++ src/cmd_vel_web_relay/scripts/cmd_vel_relay.py
@@ -1,38 +1,45 @@
 #!/usr/bin/env python3
-
 import rospy
 from geometry_msgs.msg import Twist
 from std_srvs.srv import SetBool, SetBoolResponse
 
 class CmdVelRelay:
     def __init__(self):
-        rospy.init_node("cmd_vel_relay")
+        rospy.init_node('cmd_vel_relay', anonymous=True)
 
-        self.enabled = False
+        self.sub = rospy.Subscriber('/cmd_vel_web', Twist, self.cmd_vel_callback, queue_size=1)
+        self.pub = rospy.Publisher('/hoverboard_velocity_controller/cmd_vel', Twist, queue_size=1)
+
         self.latest_cmd = Twist()
+        self.joystick_active = False     # Is joystick sending commands
+        self.relay_enabled = False       # Controlled via service
+        self.rate = rospy.Rate(20)
 
-        rospy.Subscriber("/cmd_vel_web", Twist, self.cmd_callback)
-        self.cmd_pub = rospy.Publisher("/hoverboard_velocity_controller/cmd_vel", Twist, queue_size=1)
+        # Add service
+        self.relay_service = rospy.Service('/cmd_relay_enable', SetBool, self.set_relay_status)
 
-        # Services to control relay
-        rospy.Service("/cmd_relay_enable", SetBool, self.handle_enable)
+    def set_relay_status(self, req):
+        self.relay_enabled = req.data
+        status = "enabled" if req.data else "disabled"
+        rospy.loginfo(f"🔁 Relay manually {status}")
+        return SetBoolResponse(success=True, message=f"Relay {status}")
 
-        self.rate = rospy.Rate(20)  # 20Hz
-
-    def handle_enable(self, req):
-        self.enabled = req.data
-        msg = "Relay enabled" if req.data else "Relay disabled"
-        rospy.loginfo(msg)
-        return SetBoolResponse(success=True, message=msg)
-
-    def cmd_callback(self, msg):
-        self.latest_cmd = msg
+    def cmd_vel_callback(self, msg):
+        # If message is all zeros, consider joystick inactive
+        if msg.linear.x == 0 and msg.linear.y == 0 and msg.angular.z == 0:
+            self.joystick_active = False
+        else:
+            self.joystick_active = True
+            self.latest_cmd = msg
 
     def run(self):
         while not rospy.is_shutdown():
-            if self.enabled:
-                self.cmd_pub.publish(self.latest_cmd)
+            if self.relay_enabled and self.joystick_active:
+                self.pub.publish(self.latest_cmd)
             self.rate.sleep()
 
 if __name__ == '__main__':
-    CmdVelRelay().run()
+    try:
+        CmdVelRelay().run()
+    except rospy.ROSInterruptException:
+        pass

32d319897e9f2a2353d5aece062078e81f46af0f
duc <thienductang@gmail.com>
2025-06-27 12:17:38 +0700

    urdfv6

--- src/cmd_vel_web_relay/scripts/cmd_vel_relay.py
+++ src/cmd_vel_web_relay/scripts/cmd_vel_relay.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python3
+
 import rospy
 from geometry_msgs.msg import Twist
+from std_srvs.srv import SetBool, SetBoolResponse
 
 class CmdVelRelay:
     def __init__(self):
-        rospy.init_node('cmd_vel_relay', anonymous=True)
+        rospy.init_node("cmd_vel_relay")
 
-        self.sub = rospy.Subscriber('/cmd_vel_web', Twist, self.cmd_vel_callback, queue_size=1)
-        self.pub = rospy.Publisher('/hoverboard_velocity_controller/cmd_vel', Twist, queue_size=1)
-
+        self.enabled = False
         self.latest_cmd = Twist()
-        self.should_publish = False
-        self.rate = rospy.Rate(20)
 
-    def cmd_vel_callback(self, msg):
-        # Check if all components are zero
-        if (msg.linear.x == 0 and msg.linear.y == 0 and msg.angular.z == 0):
-            self.should_publish = False
-        else:
-            self.should_publish = True
-            self.latest_cmd = msg
+        rospy.Subscriber("/cmd_vel_web", Twist, self.cmd_callback)
+        self.cmd_pub = rospy.Publisher("/hoverboard_velocity_controller/cmd_vel", Twist, queue_size=1)
+
+        # Services to control relay
+        rospy.Service("/cmd_relay_enable", SetBool, self.handle_enable)
+
+        self.rate = rospy.Rate(20)  # 20Hz
+
+    def handle_enable(self, req):
+        self.enabled = req.data
+        msg = "Relay enabled" if req.data else "Relay disabled"
+        rospy.loginfo(msg)
+        return SetBoolResponse(success=True, message=msg)
+
+    def cmd_callback(self, msg):
+        self.latest_cmd = msg
 
     def run(self):
         while not rospy.is_shutdown():
-            if self.should_publish:
-                self.pub.publish(self.latest_cmd)
+            if self.enabled:
+                self.cmd_pub.publish(self.latest_cmd)
             self.rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        node = CmdVelRelay()
-        node.run()
-    except rospy.ROSInterruptException:
-        pass
+    CmdVelRelay().run()
